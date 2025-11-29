ffb7d2750de2ad2a0f5538ee27aeac358e9e678a
localStorage <localstorage64@gmail.com>
2025-11-29 12:03:50 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
-// config.js
-const firebaseConfig = {
+// Firebase config - gerekliyse kendi projenizin değerleriyle değiştirin
+export const firebaseConfig = {
   apiKey: "AIzaSyByq_qC2_RC0xhCDEuKJ4jP7z0GHuuxXN0",
   authDomain: "localstorage-f4705.firebaseapp.com",
-  databaseURL: "https://yhtu-c13b9-default-rtdb.firebaseio.com", // 🔹 burayı ekledin
+  databaseURL: "https://localstorage-f4705-default-rtdb.firebaseio.com",
   projectId: "localstorage-f4705",
-  storageBucket: "localstorage-f4705.firebasestorage.app",
+  storageBucket: "localstorage-f4705.appspot.com",
   messagingSenderId: "682260338656",
   appId: "1:682260338656:web:c5571f07bfcc78889c7d85",
   measurementId: "G-NPM9BSFZM6"
